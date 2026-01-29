e92eb94c87cf09511cc06abb0f6577fe5d67a024
divyasrik-18 <divyasri.k@neogeoinfo.com>
2026-01-29 16:17:17 +0530

    Updated sql query

--- ui/src/index.js
+++ ui/src/index.js
@@ -14,4 +14,6 @@ root.render(
 );
 
 // 2. CHANGE THIS LINE FROM unregister() TO register()
-serviceWorkerRegistration.register();
+// serviceWorkerRegistration.register();
+// index.js
+serviceWorkerRegistration.unregister();
